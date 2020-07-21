2f00b6d469286849d823f54c865379521ce09264
Ruzgar Erik <ruzgarerik@gmail.com>
2020-07-21 03:30:24 +0300

    Add files via upload

--- burdurbot.py
+++ burdurbot.py
@@ -11,22 +11,44 @@ reddit = praw.Reddit(
     user_agent='u/ruzgarerik Bot')
 
 
-
 while True:
 	for submission in reddit.subreddit("KGBTR").new(limit=100):
-	    author = submission.author
-	    print(author)
-	    url = "https://www.reddit.com/user/{}/top_karma_subreddits.json"
-	    r = requests.get(url.format(author), headers = {'User-agent': 'u/ruzgarerik bot'})
-	    data = r.json()
-	    burdurlu = "burdurland" in str(data)
-	    if burdurlu == True:
-	    	submission.reply('''
+		sayac = 0
+		sayac2=0 
+		linkler = []
+		author = submission.author
+		print(author)
+		url = "https://www.reddit.com/user/{}/top_karma_subreddits.json"
+		r = requests.get(url.format(author), headers = {'User-agent': 'u/ruzgarerik bot'})
+		data = r.json()
+		burdurlu = "burdurland" in str(data)
+
+		if burdurlu == True:
+			s = requests.get('https://api.pushshift.io/reddit/submission/search/?author={}&subreddit=burdurland'.format(author))
+			submissiondata = s.json()
+			sayac=0
+			sayac2=0
+			linkler = []
+			for element in submissiondata['data']:
+				linkler.append(element['full_link'])
+				sayac=sayac+1
+			c = requests.get('https://api.pushshift.io/reddit/comment/search/?author={}&subreddit=burdurland'.format(author))
+			commentdata = c.json()
+			for element in commentdata['data']:
+				sayac2=sayac2+1
+			submission.reply('''
 # Burdurlu Normie Tespit Edildi !
 ***
-Bu Yorum Burdurlu Bulucu Bot Tarafından Yapılmıştır
-	    		''')
-	    	print("Görev tamamlandı bulgurlu bildirildi")
-	    else:
-	    	print(author, "bulgursuz")
-	    time.sleep(60) 
+# Bu Kişinin r/burdurland subredditindeki paylaşımları:
+{}
+***
+## {} paylaşım yapmış
+## {} yorum yapmış
+***
+# Bu Yorum Burdurlu Bulucu Bot Tarafından Yapılmıştır
+***
+'''.format(linkler,sayac,sayac2))
+			print("Görev tamamlandı bulgurlu bildirildi")
+		else:
+			print(author, "bulgursuz")
+		time.sleep(5) 
