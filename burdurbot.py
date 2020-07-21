ea6841ad463afde62b864fc12c34ce23fc42a0e9
Ruzgar Erik <ruzgarerik@gmail.com>
2020-07-21 16:09:29 +0300

    Update burdurbot.py

--- burdurbot.py
+++ burdurbot.py
@@ -2,7 +2,13 @@ import requests
 import json
 import praw
 import time
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 from os import environ
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('google-credentials.json', scope)
+client = gspread.authorize(creds)
+worksheet = client.open('BurdurBot').sheet1
 reddit = praw.Reddit(
     client_id=environ['client_id'],
     client_secret=environ['client_secret'],
@@ -10,25 +16,27 @@ reddit = praw.Reddit(
     password=environ['password'],
     user_agent='u/ruzgarerik Bot')
 
-
 while True:
-	for submission in reddit.subreddit("KGBTR").new(limit=100):
+	for submission in reddit.subreddit("KGBTR").new(limit=20):
 		sayac = 0
 		sayac2=0 
 		linkler = []
+		reply = 0
+		yorumlar = []
 		author = submission.author
 		print(author)
 		url = "https://www.reddit.com/user/{}/top_karma_subreddits.json"
 		r = requests.get(url.format(author), headers = {'User-agent': 'u/ruzgarerik bot'})
 		data = r.json()
 		burdurlu = "burdurland" in str(data)
-
 		if burdurlu == True:
 			s = requests.get('https://api.pushshift.io/reddit/submission/search/?author={}&subreddit=burdurland'.format(author))
 			submissiondata = s.json()
 			sayac=0
 			sayac2=0
+			reply = 0
 			linkler = []
+			yorumlar = []
 			for element in submissiondata['data']:
 				linkler.append(element['full_link'])
 				sayac=sayac+1
@@ -36,19 +44,41 @@ while True:
 			commentdata = c.json()
 			for element in commentdata['data']:
 				sayac2=sayac2+1
-			submission.reply('''
+				yorumlar.append(element['body'])
+			sid = submission.id
+			try:
+				cell = worksheet.find(sid)
+				if cell:
+					reply = 1
+	
+			except:
+				print("id veritabanında yok")
+				reply = 0
+
+			if reply == 0:
+				submission.reply('''
 # Burdurlu Normie Tespit Edildi !
 ***
-# Bu Kişinin r/burdurland subredditindeki paylaşımları:
+# Bu Kişinin burdurland subredditindeki paylaşımları:
+{}
+***
+# Bu kişinin burdurland subredditindeki yorumları:
 {}
 ***
 ## {} paylaşım yapmış
 ## {} yorum yapmış
 ***
-# Bu Yorum Burdurlu Bulucu Bot Tarafından Yapılmıştır
+
+Bip Bop. Ben Burdurlu Bulucu Bot. 
+[Beni Yapan Kişi](https://www.reddit.com/user/ruzgarerik) [Sorun Bildir](https://www.reddit.com/message/compose/?to=PeriodicDioxide)
+
 ***
-'''.format(linkler,sayac,sayac2))
-			print("Görev tamamlandı bulgurlu bildirildi")
+'''.format(linkler,yorumlar,sayac,sayac2))
+				print("Görev tamamlandı bulgurlu bildirildi")
+				worksheet.append_row([sid])
+
 		else:
 			print(author, "bulgursuz")
-		time.sleep(30) 
+		time.sleep(2)
+	print("Soğuma zamanı")
+	time.sleep(100)
