b3b57b59c3945321ffb354d363cda6171bf8f6c1
Asim-Tahir <a.karakus@pa.ag>
2021-11-14 20:14:57 +0300

    draft: docker-support

--- burdurbot.py
+++ burdurbot.py
@@ -5,118 +5,103 @@ import time
 import gspread
 import datetime
 from oauth2client.service_account import ServiceAccountCredentials
-import traceback
+from os import environ
+from datetime import date
 
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('key.json', scope)
+scope = [
+  "https://spreadsheets.google.com/feeds",
+  "https://www.googleapis.com/auth/drive",
+]
+creds = ServiceAccountCredentials.from_json_keyfile_name("key.json", scope)
 client = gspread.authorize(creds)
-worksheet = client.open('BurdurBot').sheet1
-worksheet2 = client.open('BOT').sheet1
-
-karaliste = ("PeriodicDioxide","KrgzBey","CrayzEbabil3","Yavuzardaergul2","HolyAyran","kahnivore","Nonstol")
-reddit = praw.Reddit(
-    client_id='',
-    client_secret='',
-    username='PeriodicDioxide',
-    password='',
-    user_agent='u/ruzgarerik Bot')
+worksheet = client.open("BurdurBot").sheet1
+karaliste = ""
+reddit = praw.Reddit("CONFIG")
 while True:
-	try:
-		for submission in reddit.subreddit("KGBTR").new(limit=10):
-			sayac = 0
-			sayac2=0 
-			linkler = []
-			reply = 0
-			yorumlar = []
-			ensonposttarih=""
-			ensonyorumtarih=""
-			author = str(submission.author)
-			url = "https://www.reddit.com/user/{}/top_karma_subreddits.json"
-			r = requests.get(url.format(author), headers = {'User-agent': 'u/ruzgarerik bot'})
-			data = r.json()
-			burdurlu = "burdurland" in str(data)
-			if burdurlu == True:
-				print("----------------------------------------")
-				print(author,"burdurlu")
-				s = requests.get('https://api.pushshift.io/reddit/submission/search/?author={}&subreddit=burdurland'.format(author))
-				submissiondata = s.json()
-				sayac=0
-				sayac2=0
-				reply = 0
-				linkler = []
-				yorumlar = []
-				for element in submissiondata['data']:
-					sayac=sayac+1
-				try:
-					ensonpost = submissiondata['data'][0]['created_utc']
-					ensonposttarih = datetime.datetime.fromtimestamp(int(ensonpost))
-				except:
-					print(author,"postu yok")
-					ensonposttarih = ""
-				c = requests.get('https://api.pushshift.io/reddit/comment/search/?author={}&subreddit=burdurland'.format(author))
-				commentdata = c.json()
-				for element in commentdata['data']:
-					sayac2=sayac2+1
-
-
-				try:
-					ensonyorum = commentdata['data'][0]['created_utc']
-					ensonyorumtarih = datetime.datetime.fromtimestamp(int(ensonyorum))
-
-				except Exception:
-					print(author," yorumu yok")
-					ensonyorumtarih = ""
-					traceback.print_exc()
-					
-				sid = submission.id
-				try:
-					cell = worksheet.find(sid)
-					if cell:
-						reply = 1
-						print(author, "id veritabanında var")
-						print("----------------------------------------")
-						tarih = datetime.date.today()
-						worksheet2.append_row([sid,author,str(tarih)])
-		
-				except Exception:
-					print(author,"id veritabanında yok")
-					reply = 0
-					traceback.print_exc()
-
-
-				try:
-					if reply == 0:
-						if author in karaliste:
-							reply=1
-							print(author,"kara listede cevap verilmiyor")
-							print("----------------------------------------")
-							tarih = datetime.date.today()
-							worksheet2.append_row([sid,author,str(tarih)])
-						else:
-							reply=0
-							print(author,"kara listede değil")
-				except Exception:
-					print("hata")
-					traceback.print_exc()
-
-
-				try:
-					if reply == 0:
-						if sayac <= 2 and sayac2 <= 10:
-							reply = 1
-							print(author, "şart yerine geldi az bulgurlu")
-							print("----------------------------------------")
-							tarih = datetime.date.today()
-							worksheet2.append_row([sid,author,str(tarih)])
-						else:
-							print(author, "çok bulgurlu")
-				except Exception:
-					traceback.print_exc()
-					print("hata")
-
-
-				if reply == 0:
-					submission.reply('''
+    try:
+        for submission in reddit.subreddit("KGBTR").new(limit=10):
+            sayac = 0
+            sayac2 = 0
+            linkler = []
+            reply = 0
+            yorumlar = []
+            ensonposttarih = ""
+            ensonyorumtarih = ""
+            author = str(submission.author)
+            url = "https://www.reddit.com/user/{}/top_karma_subreddits.json"
+            r = requests.get(url.format(author), headers={"User-agent": ""})
+            data = r.json()
+            burdurlu = "burdurland" in str(data)
+            if burdurlu == True:
+                print("----------------------------------------")
+                print(author, "burdurlu")
+                s = requests.get(
+                    "https://api.pushshift.io/reddit/submission/search/?author={}&subreddit=burdurland&limit=100".format(
+                        author
+                    )
+                )
+                submissiondata = s.json()
+                sayac = 0
+                sayac2 = 0
+                reply = 0
+                linkler = []
+                yorumlar = []
+                for element in submissiondata["data"]:
+                    sayac = sayac + 1
+                try:
+                    ensonpost = submissiondata["data"][0]["created_utc"]
+                    ensonposttarih = datetime.datetime.fromtimestamp(int(ensonpost))
+                except:
+                    print(author, "postu yok")
+                    ensonposttarih = ""
+                c = requests.get(
+                    "https://api.pushshift.io/reddit/comment/search/?author={}&subreddit=burdurland&limit=100".format(
+                        author
+                    )
+                )
+                commentdata = c.json()
+                for element in commentdata["data"]:
+                    sayac2 = sayac2 + 1
+                try:
+                    ensonyorum = commentdata["data"][0]["created_utc"]
+                    ensonyorumtarih = datetime.datetime.fromtimestamp(int(ensonyorum))
+                except:
+                    print(author, " yorumu yok")
+                    ensonyorumtarih = ""
+                sid = submission.id
+                try:
+                    cell = worksheet.find(sid)
+                    if cell:
+                        reply = 1
+                        print(author, "id veritabanında var")
+                        print("----------------------------------------")
+                except:
+                    print(author, "id veritabanında yok")
+                    reply = 0
+                try:
+                    if reply == 0:
+                        if author in karaliste:
+                            reply = 1
+                            print(author, "kara listede cevap verilmiyor")
+                            print("----------------------------------------")
+                        else:
+                            reply = 0
+                            print(author, "kara listede değil")
+                except:
+                    print("hata")
+                try:
+                    if reply == 0:
+                        if sayac <= 2 and sayac2 <= 5:
+                            reply = 1
+                            print(author, "şart yerine geldi az bulgurlu")
+                            print("----------------------------------------")
+                        else:
+                            print(author, "çok bulgurlu")
+                except:
+                    print("hata")
+                if reply == 0:
+                    submission.reply(
+                        """
 # Burdurlu Normie Tespit Edildi !
 ***
 ***
@@ -129,36 +114,23 @@ while True:
 ^En ^Son ^Yorum ^Tarihi {} GMT+0
 ***
 ^Bip ^Bop. ^Ben ^Burdurlu ^Bulucu ^Bot.
-[KGBTR Burdurlu Kullanıcılar Veritabanı ](https://kgbtrburdurlular.herokuapp.com/)
 ***
-	'''.format(sayac,ensonposttarih,sayac2,ensonyorumtarih))
-					print("Görev tamamlandı bulgurlu bildirildi")
-					print("----------------------------------------")
-					tarih = datetime.date.today()
-					worksheet.append_row([sid,author,str(tarih)])
-					worksheet2.append_row([sid,author,str(tarih)])
-
-
-			else:
-				sid = submission.id
-				print(author,"burdurlu değil")
-				try:
-					cell = worksheet2.find(sid)
-					if cell:
-						reply = 1
-						print(author, "id veritabanında var temiz")
-					else:
-						reply = 0
-				except:
-					reply = 0
-				if reply == 0:
-					tarih = datetime.date.today()
-					worksheet2.append_row([sid,author,str(tarih)])
-		print("++++++++++++++++++++++++++++++++++++++++")
-		print("ilk 10 bitti")
-		print("++++++++++++++++++++++++++++++++++++++++")
-	except Exception:
-		print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-		print("                 HATA                   ")
-		traceback.print_exc()
-		print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+[Detaylı Rapor](https://burdurlukontrol.herokuapp.com/user?id={})
+        """.format(
+                            sayac, ensonposttarih, sayac2, ensonyorumtarih, author
+                        )
+                    )
+                    print("Görev tamamlandı bulgurlu bildirildi")
+                    print("----------------------------------------")
+                    tarih = date.today()
+                    worksheet.append_row([sid, author, str(tarih)])
+            else:
+                print(author, "burdurlu değil")
+                time.sleep(5)
+        print("++++++++++++++++++++++++++++++++++++++++")
+        print("ilk 10 bitti")
+        print("++++++++++++++++++++++++++++++++++++++++")
+    except:
+        print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+        print("                 HATA                   ")
+        print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
